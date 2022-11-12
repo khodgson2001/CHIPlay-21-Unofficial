3e8fb065c834667af66318e780807ae821140a2e
Kieran <khodgson2001@gmail.com>
2022-11-12 19:13:36 +0000

    Initial Database File

---
+++ api/src/database.php
@@ -0,0 +1,34 @@
+<?php
+
+class Database
+{
+    private $dbConnection;
+
+    public function __construct($dbName)
+    {
+        $this->setDbConnection($dbName);
+    }
+
+    public function setDbConnection($dbName)
+    {
+        try {
+            $this->dbConnection = new PDO('sqlite:' . $dbName);
+            $this->dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Database Connection Error: " . $e->getMessage();
+            exit();
+        }
+    }
+
+    public function getDbConnection()
+    {
+        return $this->dbConnection;
+    }
+    
+    public function executeSQL($sql, $params = [])
+    {
+        $stmt = $this->dbConnection->prepare($sql);
+        $stmt->execute($params);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
